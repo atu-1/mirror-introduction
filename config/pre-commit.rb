a2651426740a1988b31a17ca6500d64ca50d3f06
Nutti <nutti.metro@gmail.com>
2016-10-17 23:08:16 +0900

    hoge

--- config/pre-commit.rb
+++ config/pre-commit.rb
@@ -1,8 +1,13 @@
+require 'fileutils'
+
+src_path = '_sample/'
+
 print 'aa'
 
 ARGV.all? do |filename|
     print filename
     if File.extname(filename) == '.py' then
+        FileUtils.cp(filename, dest + filename)
         print filename
     end
 end
