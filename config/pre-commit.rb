6b65d7545705a111cc44227c7b68154a20c49494
Nutti <nutti.metro@gmail.com>
2016-10-18 18:34:26 +0900

    test

--- config/pre-commit.rb
+++ config/pre-commit.rb
@@ -6,10 +6,13 @@ dest_path = 'sample/'
 ARGV.all? do |filepath|
     if File.extname(filepath) == '.py' then
         path = filepath.dup
-        path.slice!(/^\s*\/\/! \[.*\]/);
-        print path
-        #FileUtils.cp(filepath, dest_path + File.basename(filepath))
-        o
+        path.slice!(src_path)
+        src_file = File.open(filepath, 'r')
+        dest_file = File.open(dest_path + path, 'w')
+        src_file.each_line do |line|
+            next line if /^\s*\/\/! \[.*\]/ =~ line
+            dest_file.puts(line)
+        end
     end
 end
 
