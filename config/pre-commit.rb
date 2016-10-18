9af1abbeb1ccb8a4318a23eef190ee053a3c7f24
Nutti <nutti.metro@gmail.com>
2016-10-18 18:47:50 +0900

    test

--- config/pre-commit.rb
+++ config/pre-commit.rb
@@ -1,18 +1,21 @@
 require 'fileutils'
 
-src_path = 'sample_raw/'
-dest_path = 'sample/'
+raw_path = 'sample_raw/'
+target_path = 'sample/'
+
 
 ARGV.all? do |filepath|
     next filepath if filepath.match(/^sample\//)
     next filepath if File.extname(filepath) != '.py'
+
     path = filepath.dup
-    path.slice!(src_path)
+    path.slice!(raw_path)
+
+    # make clean source
     src_file = File.open(filepath, 'r')
-    dest_file = File.open(dest_path + path, 'w')
+    dest_file = File.open(target_path + path, 'w')
     src_file.each_line do |line|
         next line if /^\s*\/\/! \[.*\]/ =~ line
-        puts line
         dest_file.puts(line)
     end
 end
