6e6e351576a10a1c4cd88ac712259e058be2890a
Nutti <nutti.metro@gmail.com>
2017-03-11 21:36:49 +0900

    test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -595,4 +595,6 @@ if __name__ == "__main__":
     suite = unittest.TestSuite()
     for case in test_cases:
         suite.addTest(unittest.makeSuite(case))
-    unittest.TextTestRunner().run(suite)
+    ret = unittest.TextTestRunner().run(suite)
+
+    print(ret)
