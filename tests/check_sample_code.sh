9cce1b3ba28359021c2b22a83ca34f12143dc290
Nutti <nutti.metro@gmail.com>
2017-03-26 16:23:04 +0900

    test

--- tests/check_sample_code.sh
+++ tests/check_sample_code.sh
@@ -11,9 +11,17 @@ files=`find ${tgt} -name "*.py"`
 # pylint
 for file in ${files[@]}; do
     pylint ${file}
+    ret=`echo $?`
+    if [ ${ret} -ne 0 ]; then
+        exit 1
+    fi
 done
 
 # pep8
 for file in ${files[@]}; do
     pep8 ${file}
+    ret=`echo $?`
+    if [ ${ret} -ne 0 ]; then
+        exit 1
+    fi
 done
